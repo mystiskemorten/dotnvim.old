cc726034bfdfd449cba27b85a5e1b81be58bfd88
mystiskemorten <chrihh@proton.me>
2023-07-03 20:17:07 +0200

    add arrows for gruvbox look

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,7 +2,10 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'gruvbox',
-    component_separators = '|',
-    section_separators = '',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_c = { { 'filename', path = 1 } }
   },
 }
