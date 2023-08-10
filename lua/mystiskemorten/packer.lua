9423fc29e3a03b6a887fe368372e31d2dbde3b80
mystiskemorten <chrihh@proton.me>
2023-08-10 20:59:00 +0200

    add current buffer search

--- lua/mystiskemorten/packer.lua
+++ lua/mystiskemorten/packer.lua
@@ -51,7 +51,7 @@ return require('packer').startup(function(use)
 	use 'ray-x/guihua.lua'
 
 	-- Fuzzy Finder (files, lsp, etc)
-	use { 'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	use { 'nvim-telescope/telescope.nvim', tag = '0.1.2',
 		-- or                            , branch = '0.1.x',
 		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
