39b268491d42b609ea5c44c3f33b831a69a8f090
mystiskemorten <chrihh@proton.me>
2023-09-17 12:51:15 +0200

    use similar git binds and user's terminal

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -1,3 +1,3 @@
-vim.api.nvim_set_keymap("n", "<leader>go", ":DiffviewOpen<cr>", { silent = true, noremap = true, desc = '[G]it diffview [o]pen'})
+vim.api.nvim_set_keymap("n", "<leader>gv", ":DiffviewOpen<cr>", { silent = true, noremap = true, desc = '[G]it diffview [o]pen'})
 vim.api.nvim_set_keymap("n", "<leader>gq", ":DiffviewClose<cr>", { silent = true, noremap = true, desc = '[G]it diffview [q]uit'})
-vim.api.nvim_set_keymap("n", "<leader>gh", ":DiffviewFileHistory<cr>", { silent = true, noremap = true, desc = '[G]it diffview file [h]istory'})
+vim.api.nvim_set_keymap("n", "<leader>gl", ":DiffviewFileHistory<cr>", { silent = true, noremap = true, desc = '[G]it diffview file [h]istory'})
--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gd", vim.cmd.Git);
 
 local Mystiskemorten_Fugutive = vim.api.nvim_create_augroup("Mystiskemorten_Fugutive", {})
 
